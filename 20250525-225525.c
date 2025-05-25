449098e5d733d449e49d928eb5ff0c7df2f8f7b4
Chaelwop <michaelveracruz45@gmail.com>
2025-05-25 22:55:45 +0800

    Add files via upload

---
+++ 20250525-225525.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main () {
+ int myNum = 8;
+ float myFloat = 8.99;
+ char myLetter = 'M';
+ printf("My score in math test was: %d\n", myNum);
+ printf("My average last year was: %f\n", myFloat);
+ printf("My first name start in: %c\n", myLetter);
+ printf("Hello world!\n");
+
+ int x = 1;
+ int y = 2;
+ int sum = x + y;
+ printf("%d\n", sum );
+ 
+ printf("My average last year was: %f\n", 89.000);
+ printf("My score in Math was: %d\n", 10);
+ printf("My score in math: %d\nMy average last year was: %f\nMy first leter was: %c", 10,89.000,'M');
+ return 0;
+ 
+ }
+
+    
